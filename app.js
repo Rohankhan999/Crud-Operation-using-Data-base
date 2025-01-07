0f736465e0c64f36ad528747942a3d32c3b6ed8f
Rohankhan999 <rohankhan08453@gmail.com>
2025-01-08 01:14:38 +0500

    .

---
+++ app.js
@@ -0,0 +1,12 @@
+//   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+
+
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyAWn3ZBtNwLnZ3_FHxOExe3XEYW5nDfK_E",
+//     authDomain: "crud-operation-999.firebaseapp.com",
+//     projectId: "crud-operation-999",
+//     storageBucket: "crud-operation-999.firebasestorage.app",
+//     messagingSenderId: "704654946310",
+//     appId: "1:704654946310:web:592d6e03f6b550c6bb5387"
+//   };
+//   const app = initializeApp(firebaseConfig);
