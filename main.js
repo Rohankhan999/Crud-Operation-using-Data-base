89fe36d1e60d8d4255e70b1a63807be16011672f
Rohankhan999 <rohankhan08453@gmail.com>
2025-01-11 00:59:42 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAuth, onAuthStateChanged  } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 const firebaseConfig = {
-  apiKey: "AIzaSyAWn3ZBtNwLnZ3_FHxOExe3XEYW5nDfK_E",
-  authDomain: "crud-operation-999.firebaseapp.com",
-  projectId: "crud-operation-999",
-  storageBucket: "crud-operation-999.firebasestorage.app",
-  messagingSenderId: "704654946310",
-  appId: "1:704654946310:web:592d6e03f6b550c6bb5387"
-};
+    apiKey: "AIzaSyAy2sdXZBATUG899y7KZpZsnOluIckd_Jc",
+    authDomain: "login-signup-999.firebaseapp.com",
+    projectId: "login-signup-999",
+    storageBucket: "login-signup-999.firebasestorage.app",
+    messagingSenderId: "831110980438",
+    appId: "1:831110980438:web:0974f24c997274899d2cf7"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
+  const app = initializeApp(firebaseConfig);
+//   const username = document.getElementById("displayUser");
+//   const email = document.getElementById("displayEmail");
+//  const Phoneno = document.getElementById("displayPhone");
+    const auth = getAuth(app);
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+        
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Data Failed",
+            text: "User Sign Out",
+        });
+    }
+  });
